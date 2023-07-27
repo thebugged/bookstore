fa42d28976f4b1c95f9027ac693c639fe2b36ad0
Maikyau Israel <maikyauisrael@gmail.com>
2023-07-27 13:20:22 +0100

    update commit

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-anw5!fzae&3hx@c4u0!e%-zw6+281!^ts$#m+&d(wwj8!%w*)#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -75,12 +75,24 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'UoZSSQ4MsVbuDSLbaJPH',
+        'HOST': 'containers-us-west-179.railway.app',
+        'PORT': '7157',
     }
 }
+
 
 
 # Password validation
@@ -126,6 +138,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/images/'
 
