510e26c62b9f7fb03cf0dc0fc5f9a3f5e0040da1
Maikyau Israel <maikyauisrael@gmail.com>
2024-04-12 12:58:44 +0100

    Fix csrftoken error and updates for deployment

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-anw5!fzae&3hx@c4u0!e%-zw6+281!^ts$#m+&d(wwj8!%w*)#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
